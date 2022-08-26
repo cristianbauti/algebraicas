ec24850f7e14ccac4d858cbd0a0dbd5698a173f7
cristianbauti <111815884+cristianbauti@users.noreply.github.com>
2022-08-26 17:35:01 -0500

    Add files via upload

---
+++ ope_matrices.m
@@ -0,0 +1,27 @@
+close all
+clear all
+clc
+
+a=[2 5 9 5 2;
+    5 4 5 2 5;
+    2 6 8 5 2; 
+    2 4 5 6 2;
+    9 3 5 8 6];
+
+b=[3 5 4 7 6;
+   6 1 8 2 5;
+   3 4 7 7 6;
+   8 5 9 3 5;
+   9 5 2 4 3];
+
+suma=(a+b);
+
+resta=(a-b);
+
+mutiplicacion=a*b
+
+a(2;5,1;3)
+
+determinantre=det(b)
+
+matriz_inv=inv(a)
